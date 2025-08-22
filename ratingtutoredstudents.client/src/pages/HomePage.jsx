713f276d4d63da8fc98951f23d4f8aef7489a86a
Carlos Cruz <carlosjoecruz@gmail.com>
2025-08-22 18:14:22 -0500

    Getting Initial frontendpage setup, now need to work on getting database connected and loading data to table. Still need to create table

--- ratingtutoredstudents.client/src/pages/HomePage.jsx
+++ ratingtutoredstudents.client/src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
+import "./styles/HomePage.css"
 function HomePage() {
-  return (
-    <p>Hello world!</p>
+    return (
+        <div className="wrapper">   
+            <div className="title">
+                <p>Welcome Tutors</p>
+            </div>
+
+            <div className="select-student">
+                <p>Search for students</p>
+            </div>
+        </div>
   );
 }
 
