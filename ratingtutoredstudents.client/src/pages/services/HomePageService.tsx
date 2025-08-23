3bdd0e09202fbc4567d81f81ae29a449eb8be234
Carlos Cruz <carlosjoecruz@gmail.com>
2025-08-23 17:22:46 -0500

    Finishing getStudents() service

--- ratingtutoredstudents.client/src/pages/services/HomePageService.tsx
+++ ratingtutoredstudents.client/src/pages/services/HomePageService.tsx
@@ -1,4 +1,4 @@
-import Student from "../../types/Student"
+import type { Student } from "../../types/Student"
 
 async function getStudentsFromDb() {
     const response = await fetch("https://localhost:7137/Students/GetAllStudents");
@@ -9,7 +9,11 @@ async function getStudentsFromDb() {
 }
 
 export async function getStudents(): Promise<Student[]> {
-    const raw = await getStudentsFromDb()
+    const raw = await getStudentsFromDb();
 
-
+    return raw.map((r: any) => ({
+        id: r.id,
+        first_name: r.first_name,
+        last_name: r.last_name,
+    }));
 }
